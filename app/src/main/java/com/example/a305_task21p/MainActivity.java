6d6fd2fab5df323e1f1e9da470bb5f1584e03ba7
LidongWu <110621301+BlakeWu666@users.noreply.github.com>
2023-04-21 12:36:32 +1000

    Update MainActivity.java

--- app/src/main/java/com/example/a305_task21p/MainActivity.java
+++ app/src/main/java/com/example/a305_task21p/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     //
     Spinner spinner;
     Spinner spinner2;
+    
+    Spinner spinner3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         myInput = findViewById(R.id.editTextTextPersonName);
         spinner = findViewById(R.id.spinner);
         spinner2 = findViewById(R.id.spinner2);
+        spinner3 = findViewById(R.id.spinner3);
 
         myBtn.setOnClickListener(new View.OnClickListener(){
 
@@ -39,28 +42,72 @@ public class MainActivity extends AppCompatActivity {
 
                 int spinner_pos = spinner.getSelectedItemPosition();
                 int spinner2_pos = spinner2.getSelectedItemPosition();
+                int spinner3_pos = spinner3.getSelectedItemPosition();
 
-                if (spinner_pos == 0 && spinner2_pos == 1){
-                    double result = convertTheInput * 0.453592;
-                    Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
-                } else if (spinner_pos == 0 && spinner2_pos == 2) {
-                    double result = convertTheInput * 16;
-                    Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
-                } else if (spinner_pos == 0 && spinner2_pos == 3) {
-                    double result = convertTheInput * 453.592;
-                    Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
-                } else if (spinner_pos == 1 && spinner2_pos == 2) {
-                    double result = convertTheInput * 35.274;
-                    Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
-                } else if (spinner_pos == 1 && spinner2_pos == 3) {
-                    double result = convertTheInput * 1000;
-                    Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
-                } else if (spinner_pos == 2 && spinner2_pos == 3) {
-                    double result = convertTheInput * 28.3495;
-                    Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
-                }else{
-                    Toast.makeText(MainActivity.this,"Error..Something is wrong. You may have entered an invalid number.Please try again.",Toast.LENGTH_LONG).show();
+                if (spinner3_pos == 1){
+                    if (spinner_pos == 0 && spinner2_pos == 1){
+                        double result = convertTheInput * 0.453592;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    } else if (spinner_pos == 0 && spinner2_pos == 2) {
+                        double result = convertTheInput * 16;
+                        Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
+                    } else if (spinner_pos == 0 && spinner2_pos == 3) {
+                        double result = convertTheInput * 453.592;
+                        Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
+                    } else if (spinner_pos == 1 && spinner2_pos == 2) {
+                        double result = convertTheInput * 35.274;
+                        Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
+                    } else if (spinner_pos == 1 && spinner2_pos == 3) {
+                        double result = convertTheInput * 1000;
+                        Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
+                    } else if (spinner_pos == 2 && spinner2_pos == 3) {
+                        double result = convertTheInput * 28.3495;
+                        Toast.makeText(MainActivity.this,"Result is: " + result,Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(MainActivity.this,"Error..Something is wrong. You may have entered an invalid number.Please try again.",Toast.LENGTH_LONG).show();
+                    }
+
                 }
+                else if (spinner3_pos == 0){
+                    if(spinner_pos == 4 && spinner2_pos == 5){
+                        double result = convertTheInput * 2.54;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    } else if (spinner_pos == 6 && spinner2_pos == 5) {
+                        double result = convertTheInput * 30.48;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    }else if (spinner_pos == 7 && spinner2_pos == 5) {
+                        double result = convertTheInput * 91.44;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    }else if (spinner_pos == 8 && spinner2_pos == 9) {
+                        double result = convertTheInput * 1.60934;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(MainActivity.this,"Error..Something is wrong. You may have entered an invalid number.Please try again.",Toast.LENGTH_LONG).show();
+                    }
+                }
+                else if (spinner3_pos == 2){
+                    if(spinner_pos == 10 && spinner2_pos == 11){
+                        double result = convertTheInput * 1.8 + 32;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    } else if (spinner_pos == 11 && spinner2_pos == 10) {
+                        double result = (convertTheInput - 32 ) / 1.8;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    }else if (spinner_pos == 10 && spinner2_pos == 12) {
+                        double result = convertTheInput + 273.15;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    }else if (spinner_pos == 12 && spinner2_pos == 10) {
+                        double result = convertTheInput - 273.15;
+                        Toast.makeText(MainActivity.this,"Result is:" + result, Toast.LENGTH_LONG).show();
+                    }else{
+                        Toast.makeText(MainActivity.this,"Error..Something is wrong. You may have entered an invalid number.Please try again.",Toast.LENGTH_LONG).show();
+                    }
+
+                }
+                else {
+                    Toast.makeText(MainActivity.this,"Please enter a valid number and try again..",Toast.LENGTH_LONG).show();
+                }
+
+
             }
         });
 
@@ -81,6 +128,13 @@ public class MainActivity extends AppCompatActivity {
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinner2.setAdapter(adapter2);
+
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,
+                R.array.Conversions, android.R.layout.simple_spinner_item);
+
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner3.setAdapter(adapter3);
 
 
     }// By Lidong Wu
